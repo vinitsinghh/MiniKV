22b40088ec79dd4991230885804b1667821b04bb
vinit <vinaysingh@iitgn.ac.in>
2025-11-05 11:27:12 +0000

    implemented simple application protocol

--- server.cpp
+++ server.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,8 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+
+
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
@@ -17,17 +20,72 @@ static void die(const char *msg) {
     abort();
 }
 
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        msg("read() error");
-        return;
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
     }
-    printf("client says: %s\n", rbuf);
+    return 0;
+}
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t query(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
+    if (len > k_max_msg) {
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);  // assume little endian
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+        return err;
+    }
+
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    printf("server says: %.*s\n", len, &rbuf[4]);
+    return 0;
 }
 
 int main() {
@@ -36,37 +94,30 @@ int main() {
         die("socket()");
     }
 
-    // this is needed for most server applications
-    int val = 1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-
-    // bind
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(6380);        //port //(ntohs----> host(cpu) endian -> big endian)
-    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
-    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr)); // return value v = 0 if successful
+    addr.sin_port = ntohs(6380);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) {
-        die("bind()");
+        die("connect");
     }
 
-    // listen
-    rv = listen(fd, SOMAXCONN); //SOMAXCONN -> size of the queue(4096)
-    if (rv) {
-        die("listen()");
+    // multiple requests
+    int32_t err = query(fd, "hello1");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello2");
+    if (err) {
+        goto L_DONE;
+    }
+    err = query(fd, "hello3");
+    if (err) {
+        goto L_DONE;
     }
 
-    while (true) {
-        // accept
-        struct sockaddr_in client_addr = {};
-        socklen_t addrlen = sizeof(client_addr);
-        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-        if (connfd < 0) {
-            continue;   // error
-        }
-        do_something(connfd);
-        close(connfd);
-    }
-
+L_DONE:
+    close(fd);
     return 0;
 }
